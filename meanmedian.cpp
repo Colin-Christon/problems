3a772cb0184eaa3bd47eb60b3a93ec706f859f9b
Colin-Christon <109790053+Colin-Christon@users.noreply.github.com>
2022-09-13 15:35:47 +0530

    Update meanmedian.cpp

--- meanmedian.cpp
+++ meanmedian.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int a,b,c;
+	    b=y;
+	    if(x==y){
+	        cout<<x<<" "<<x<<" "<<x<<endl;
+	    }
+	   else{
+	      int ex=(3*x)-b;
+	      for(int i=-1000;i<=b;i++){
+	          a=i;
+	          c=ex-a;
+	          if(c<1000){
+	          cout<<a<<" "<<b<<" "<<c<<endl;
+	           break;
+	          }
+	      }
+	   }
+	    
+	}
+	return 0;
+}
